48d1e4c03501c0712a51251dbc330bfc3540d150
OpiumBird <145978078+MarcosMartinsCarvalho@users.noreply.github.com>
2025-06-20 12:36:36 +0200

    les3

---
+++ PROG-M4/Assets/opdracht3/GameManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float speed = 5f;
+    public float timeLeft = 5f;
+    private int score = 0;
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveX, 0, moveZ).normalized * speed * Time.deltaTime;
+        transform.Translate(move);
+
+        if (timeLeft <= 0)
+        {
+            Debug.Log("Game Over! Eindscore: " + score);
+            enabled = false;
+            return;
+        }
+
+        timeLeft -= Time.deltaTime;
+
+        Debug.Log("Tijd: " + Mathf.Round(timeLeft) + " | Score: " + score);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Coin")
+        {
+            score += 10;
+            Debug.Log("Munt gepakt! +10 punten");
+            Destroy(other.gameObject);
+        }
+    }
+}
